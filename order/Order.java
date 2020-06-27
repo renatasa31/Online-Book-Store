0860fbf0cdb2de1d044b4fda1079030c4353ac4b
samnunel56 <67509713+samnunel56@users.noreply.github.com>
2020-06-27 23:17:35 +0700

    Update Order Java

---
+++ order/Order.java
@@ -0,0 +1,11 @@
+public class OrderBean implements SessionBean {
+
+  public void ejbActivate() {}
+  public void ejbRemove()   {}    // end of remove
+
+  public void ejbPassivate() {}
+  public void setSessionContext(SessionContext ctx) {}
+
+  public void ejbCreate  () throws CreateException {}
+
+}
