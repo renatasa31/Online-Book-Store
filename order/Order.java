f2a11143516dd8e9c69ee9786b42b24bbecce871
Renatasa Sim <67510457+renatasa31@users.noreply.github.com>
2020-06-28 20:17:52 +0700

    Update Order

--- order/Order.java
+++ order/Order.java
@@ -1,7 +1,24 @@
+// tag::baseClass[]
+package tacos.web;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+//end::baseClass[]
+import org.springframework.web.bind.annotation.PostMapping;
+//tag::baseClass[]
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.Order;
 public class OrderBean implements SessionBean {
 
-  public void ejbActivate() {}
-  public void ejbRemove()   {}    // end of remove
+  public void ejbActivate() {
+  }
+  public void ejbRemove()   {}    
+  // end of remove
 
   public void ejbPassivate() {}
   public void setSessionContext(SessionContext ctx) {}
