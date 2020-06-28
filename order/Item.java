fe042144cf1e18ef4c7889448148e197700922c1
yessi anatasa <67510316+yesiana@users.noreply.github.com>
2020-06-28 20:11:02 +0700

    Update Item Java

--- order/Item.java
+++ order/Item.java
@@ -1,4 +1,7 @@
-// Item class
+//Item
+//Book
+//class
+
 package  obs;
 
 public class Item 
@@ -18,17 +21,46 @@ public class Item
       this.qty = 1;
    }
    
+   //untuk Memperoleh ISBN
    public String getIsbn()
    {
       return  isbn;
    }
-
+   
+   //Untuk memperoleh judul
    public String getTitle()
    {
       return  title;
    }
+   
+   //Untuk memperoleh jumlah buku
    public int getQty()
    {
       return  qty;
    }
+   
+   //Untuk memilih jumlah buku
+   public void setQty(int qty)
+   {   this.qty = qty; }
+   
+   //Untuk menambahkan jumlah buku
+   public void addQty(int qty)
+   {   this.qty += qty;  }
+   
+   //Untuk memperoleh harga
+   public int getPrice()
+   {
+      return  price;
+   }
+  } 
+// end 
+//of 
+//class
+//item
+//of
+//book
+
+
+
+
 }
