b5e1ca05c9559156731aa120df2181a533b86425
yosafat yesaya <67510154+yosafat64@users.noreply.github.com>
2020-06-28 19:36:52 +0700

    update OrderHome Page

--- order/OrderHome.java
+++ order/OrderHome.java
@@ -1,9 +1,1 @@
-package obs.order;
-
-import java.rmi.RemoteException;
-import javax.ejb.CreateException;
-import javax.ejb.EJBHome;
-
-public interface OrderHome extends EJBHome {
-   Order create() throws CreateException, RemoteException;
-}
+//Untuk bagian home page order
